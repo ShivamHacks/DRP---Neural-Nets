4e0d1014e9a18caa6111ac9e44096cbc9aa4fee6
Shivam Hacks <shivamlego@gmail.com>
2017-11-24 17:45:07 -0500

    added data generating function

--- MyNN/network3.py
+++ MyNN/network3.py
@@ -1,14 +1,9 @@
 import numpy as np
-from copy import copy, deepcopy
 from sklearn import preprocessing
 from sklearn.utils import shuffle
 from sklearn.metrics import mean_squared_error
 
 np.random.seed(10)
-
-# possibly change talk topic to:
-# Neural Networks as Polynomial Regressors
-# Using Neural Networks as a Universal Approximator
 
 class Layer(object):
 
@@ -16,9 +11,7 @@ class Layer(object):
 		self.num_inputs = num_inputs
 		self.num_neurons = num_neurons
 		self.weights = np.random.randn(num_neurons, num_inputs)
-		#print self.weights
 		self.biases = np.random.randn(num_neurons, )
-		#print self.biases
 
 	def pass_through(self, input):
 		"""outputs = []
@@ -51,7 +44,7 @@ class Network(object):
 	def cost(self, output, actual):
 		return 0.5 * np.linalg.norm(output - actual)
 
-	def train_one(self, train_x, train_y, learning_rate, eta=0.1):
+	def train_one(self, train_x, train_y, learning_rate, eta=0.01):
 
 		weight_gradients = []
 		bias_gradients = []
@@ -62,7 +55,7 @@ class Network(object):
 			layer_bias_gradients = np.zeros(self.biases[i].shape)
 
 			# CHECK HERE
-			for j in range(self.structure[i + 1]): # iterate over neuron
+			for j in range(self.structure[i + 1]): # iterate over neurons
 				
 				# calculate bias gradients for this neuron
 				self.biases[i][j] += eta
@@ -73,8 +66,6 @@ class Network(object):
 				bias_gradient = (cost_adjusted_bias_more - cost_adjusted_bias_less) / (2 * eta)
 				
 				layer_bias_gradients[j] = bias_gradient
-
-				#neuron_weight_gradients = []
 
 				for k in range(len(self.weights[i][j])):
 
@@ -87,35 +78,20 @@ class Network(object):
 					
 					layer_weight_gradients[j][k] = weight_gradient
 
-				#layer_weight_gradients.append(neuron_weight_gradients)
-
 			weight_gradients.append(layer_weight_gradients)
 			bias_gradients.append(layer_bias_gradients)
 
-		#print [weights_gradient.shape for weights, weights_gradient in zip(self.weights, weight_gradients)]
 		self.weights = [weights - learning_rate * weights_gradient for weights, weights_gradient in zip(self.weights, weight_gradients)]
 		self.biases = [biases - learning_rate * bias_gradient for biases, bias_gradient in zip(self.biases, bias_gradients)]
-
-		#self.weights -= learning_rate * weight_gradients
-		#self.biases -= learning_rate * bias_gradients
-
-		"""for i in range(len(weight_gradients)):
-			for j in range(len(weight_gradients[i])):
-				for k in range(len(weight_gradients[i][j])):
-					self.layers[i].weights[j][k] -= learning_rate * weight_gradients[i][j][k]
-		
-		for i in range(len(bias_gradients)):
-			for j in range(len(bias_gradients[i])):
-				self.layers[i].biases[j] -= learning_rate * bias_gradients[i][j]"""
 
 	def train(self, X, y, learning_rate, epochs):
 		X, y = shuffle(X, y)
 		for i in range(epochs):
 			for j in range(len(X)):
 				self.train_one(X[j], y[j], learning_rate)
-			print 'Completed Epoch: ' + str(i+1) + ' Error: ' + str(mean_squared_error(self.evaluate_multiple(X), y))
+			print 'Completed Epoch: ' + str(i+1) + ' Error: ' + str(mean_squared_error(self.predict_multiple(X), y))
 
-	def evaluate_multiple(self, input):
+	def predict_multiple(self, input):
 		output = []
 		for i in range(len(input)):
 			output.append(self.predict(input[i]))
@@ -123,45 +99,47 @@ class Network(object):
 
 
 def activation(x):
-	return np.tanh(x)
+	#return np.tanh(x)
+	return x * (x > 0) #ReLu
 
 def activation_derivative(x):
-	return 1.0 - np.tanh(x)**2
+	#return 1.0 - np.tanh(x)**2
+	return (x > 0) * 1 #ReLu
 
-# Test Network
+def generate_data(data_range, step, data_split, function_to_predict):
+	num_data_points = int( (data_range[1] - data_range[0]) / step) + 1
+	x = np.arange(data_range[0], data_range[1] + step, step).reshape(num_data_points, 1)
+	y = function_to_predict(x)
+	x, y = shuffle(x, y)
+	train_x = x[:int(num_data_points * data_split)]
+	train_y = y[:int(num_data_points * data_split)]
+	test_x = x[int(num_data_points * data_split):]
+	test_y = y[int(num_data_points * data_split):]
+	continuous_x = np.linspace(data_range[0], data_range[1], 1000)
+	return train_x, train_y, test_x, test_y, continuous_x
 
-myNN = Network([1, 3, 3, 1])
+# Build Network
 
-def func_to_predict(x): return x ** 3 + (x - 1) ** 2
+myNN = Network([1, 3, 1])
 
-dataRange = [-1.0, 1.0]
-step = 0.05
-numDataPoints = int( (dataRange[1] - dataRange[0]) / step) + 1
+def func_to_predict(x): return 50 * (x-1) ** 2
+
+train_x, train_y, test_x, test_y, continuous_x = generate_data([-1.0, 1.0], 0.01, 0.8, func_to_predict)
+
+# Train Network
 learning_rate = 0.1
-num_epochs = 100
-
-x = np.arange(dataRange[0], dataRange[1] + step, step)
-y = func_to_predict(x)
-
-train_x = np.array(x).reshape(numDataPoints, 1)
-train_y = np.array(y).reshape(numDataPoints, 1)
-
+num_epochs = 50
 myNN.train(train_x, train_y, learning_rate, num_epochs)
 
-y_test = []
-for i in range(len(train_x)):
-	y_test.append(myNN.predict(train_x[i]))
-y_test = np.array(y_test).reshape(numDataPoints, 1)
-
-# PLOT
+# Plot results
 
 import matplotlib.pyplot as plt
 
-plt.scatter(train_x, train_y, color='b')
-plt.scatter(train_x, y_test, color='r')
+plt.scatter(test_x, test_y, color='b')
+plt.scatter(test_x, myNN.predict_multiple(test_x), color='r')
 
-x_graph = np.linspace(dataRange[0], dataRange[1], 1000)
-plt.plot(x_graph, func_to_predict(x_graph), color='y')
+# Graph continuous function
+plt.plot(continuous_x, func_to_predict(continuous_x), color='y')
 
 plt.show()
 
